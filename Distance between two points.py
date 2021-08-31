d3e5c644fac7d6e780e17597616c20cda6b8d1e2
PiyushJ50703 <60563307+PiyushJ50703@users.noreply.github.com>
2021-08-31 11:02:55 +0530

    Add files via upload

---
+++ Distance between two points.py
@@ -0,0 +1,6 @@
+a1 = int(input("Enter x coordinate of 1st point: "))
+b1 = int(input("Enter y coordinate of 1st point: "))
+a2 = int(input("Enter x coordinate of 2nd point: "))
+b2 = int(input("Enter y coordinate of 2nd point: "))
+s=((a1-a2)**2+(b1-b2)**2)**0.5
+print("Distance is: ",s)
